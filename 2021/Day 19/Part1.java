15d5cee78775cf0d7820ffbdc2a0dec11f7b1011
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-12-29 18:16:34 +0530

    added comments

--- 2021/Day 19/Part1.java
+++ 2021/Day 19/Part1.java
@@ -17,6 +17,10 @@ public class Part1 {
         comparersList.offer(scanners.get(0));
         comparedToList.removeAll(comparersList);
 
+        // Comparers transform the position of comparedTo scanners based on comparer's position. For Example: Scanner 0 will change Scanner 1's position on a match.
+        // The use of queue makes sure that only those scanners are making transformation to other scanners that got initially transformed by scanner 0.
+        // By transform I mean if distance between scanner A and B is (1,2,3), then all points within scanner B is added an offset of (1,2,3) as all points in B
+        // would be an extra (1,2,3) distance away far from scanner A.
         while(!comparersList.isEmpty()) {
             Scanner comparer = comparersList.poll();
             for(int i=0; i<comparedToList.size(); i++) {
